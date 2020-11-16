965e23020566fcdf4ed5ef2f63f6491d6d026097
Rico Kahler <ricokahler@me.com>
2020-11-16 00:13:44 -0500

    add test + ci + other config

---
+++ e2e/next.test.js
@@ -0,0 +1,22 @@
+import exec from '@ricokahler/exec';
+
+jest.setTimeout(60 * 1000);
+
+it('builds the app and removes dead code via the babel plugin', async () => {
+  await exec('rm -rf test-app');
+  await exec(
+    'create-next-app test-app --example https://github.com/ricokahler/next-data-hooks --example-path examples/next-data-hooks-example -y'
+  );
+  await exec('yarn --cwd ./test-app add ../dist');
+  await exec('yarn --cwd ./test-app build');
+  await exec('yarn --cwd ./test-app export');
+
+  try {
+    await exec('grep -r ./test-app/out -e get-blog-posts-side-effect -q');
+  } catch {
+    // we expect grep to throw because it shouldn't find that code
+  }
+
+  // a control: we do expect grep to find something here
+  await exec('grep -r ./test-app/out -e lorem -q');
+});
