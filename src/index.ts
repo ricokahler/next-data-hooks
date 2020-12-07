e36e1c60d95a54a6b64f291d096adce48cd8482b
Rico Kahler <ricokahler@me.com>
2020-12-07 01:14:31 -0500

    update bundle entry point

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as createDataHook } from './create-data-hook';
-export { default as createDataHooksProps } from './create-data-hooks-props';
+export { default as getDataHooksProps } from './get-data-hooks-props';
 export { default as NextDataHooksContext } from './next-data-hooks-context';
 export { default as NextDataHooksProvider } from './next-data-hooks-provider';
